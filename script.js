f7662172d72fbc627520871c110cc1a685e22d3d
Nicolas <nccrosetti@gmail.com>
2023-03-27 16:56:58 -0300

    add pages to grass modal

--- script.js
+++ script.js
@@ -4,6 +4,13 @@ const grassModal = document.getElementById('grass-modal');
 const productsBackdrop = document.getElementById('products-backdrop');
 const closeModal = document.getElementById('close-modal');
 const grassButton = document.getElementById('grass-button');
+const toPageOne = document.getElementById('to-page-one');
+const toPageTwo = document.getElementById('to-page-two');
+//Grass Modal Navigation
+const grassModalPageOne = document.getElementById('grass-modal-page-one');
+const grassModalPageTwo = document.getElementById('grass-modal-page-two');
+const grassModalPageOneNavigation = document.getElementById('grass-modal-page-one-navigation');
+const grassModalPageTwoNavigation = document.getElementById('grass-modal-page-two-navigation');
 
 const addNavbarColor = () => {
   navbar.classList.add('bg-black');
@@ -29,9 +36,11 @@ const handleScroll = () => {
 
 window.addEventListener('scroll', handleScroll);
 
+//Grass Modal
 grassButton.addEventListener('click', () => {
   productsBackdrop.classList.remove('hidden');
   grassModal.classList.remove('hidden');
+  grassModal.classList.add('flex');
 
   window.setTimeout(() => {
     grassModal.classList.remove('opacity-0');
@@ -50,4 +59,23 @@ closeModal.addEventListener('click', () => {
       grassModal.classList.add('hidden');
     }, 500);
   
+});
+
+//Grass Modal Navigation
+toPageTwo.addEventListener('click', () => {
+  //Hide first page
+  grassModalPageOne.classList.add('hidden');
+  grassModalPageOneNavigation.classList.add('hidden');
+  //Show second page
+  grassModalPageTwo.classList.remove('hidden');
+  grassModalPageTwoNavigation.classList.remove('hidden');
+});
+
+toPageOne.addEventListener('click', () => {
+  //Show first page
+  grassModalPageOne.classList.remove('hidden');
+  grassModalPageOneNavigation.classList.remove('hidden');
+  //Hide second page
+  grassModalPageTwo.classList.add('hidden');
+  grassModalPageTwoNavigation.classList.add('hidden');
 });
