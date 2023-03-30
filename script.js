d088fafa1d602cc2f7feac5e8363e9e6cd42022a
Nicolas <nccrosetti@gmail.com>
2023-03-30 12:52:08 -0300

    fix mobile navbar and add functionalities

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
 const navbar = document.getElementById('navbar');
 const scrollPosition = () => window.pageYOffset;
-const grassModal = document.getElementById('grass-modal');
 const productsBackdrop = document.getElementById('products-backdrop');
+//mobile navbar
+const mobileNavButton = document.getElementById('mobile-nav-button');
+const mobileNav = document.getElementById('mobile-nav');
+const navbarMobile = document.getElementById('navbarMobile');
+//Grass Modal
+const grassModal = document.getElementById('grass-modal');
 const closeModal = document.getElementById('close-modal');
 const grassButton = document.getElementById('grass-button');
-const toPageOne = document.getElementById('to-page-one');
-const toPageTwo = document.getElementById('to-page-two');
+const toPageOne = document.querySelectorAll('.to-page-one');
+const toPageTwo = document.querySelectorAll('.to-page-two');
 //Grass Modal Navigation
 const grassModalPageOne = document.getElementById('grass-modal-page-one');
 const grassModalPageTwo = document.getElementById('grass-modal-page-two');
@@ -30,7 +35,7 @@ window.onload = function() {
   }
 };
 
-
+//#region navbar
 const addNavbarColor = () => {
   navbar.classList.add('bg-black');
   navbar.classList.remove('bg-transparent');
@@ -54,10 +59,36 @@ const handleScroll = () => {
 };
 
 window.addEventListener('scroll', handleScroll);
+//#endregion
+
+//#region mobile navbar
+mobileNavButton.addEventListener('click', () => {
+  mobileNav.classList.toggle('hidden');
+
+  window.setTimeout(() => {
+    mobileNav.classList.toggle('opacity-0');
+  }, 1);
+});
+window.addEventListener('scroll', () => {
+  if (window.pageYOffset > 0) {
+    navbarMobile.classList.add('opacity-50');
+  } else {
+    navbarMobile.classList.remove('opacity-50');
+  }
+});
+//#endregion
+
+const backgroundImage = document.getElementById('background-image');
+const images = ['img/agro.jpg', 'img/agro2.jpg', 'img/agro3.jpg'];
+let index = 0;
+
+setInterval(() => {
+  index = (index + 1) % images.length;
+  backgroundImage.src = images[index];
+}, 10000);
 
 
-
-//Grass Modal
+//#region Grass Modal
 grassButton.addEventListener('click', () => {
   productsBackdrop.classList.remove('hidden');
   grassModal.classList.remove('hidden');
@@ -82,22 +113,43 @@ closeModal.addEventListener('click', () => {
   
 });
 
-//Grass Modal Navigation
-toPageTwo.addEventListener('click', () => {
-  //Hide first page
-  grassModalPageOne.classList.add('hidden');
-  grassModalPageOneNavigation.classList.add('hidden');
-  //Show second page
-  grassModalPageTwo.classList.remove('hidden');
-  grassModalPageTwoNavigation.classList.remove('hidden');
+document.addEventListener('keydown', function(event) {
+  if (event.key === 'Escape') {
+    // Aquí puedes hacer lo que necesites al presionar Esc
+    grassModal.classList.add('opacity-0');
+    productsBackdrop.classList.remove('opacity-70');
+    productsBackdrop.classList.add('opacity-0');
+
+    window.setTimeout(() => {
+      productsBackdrop.classList.add('hidden');
+      grassModal.classList.add('hidden');
+    }, 500);
+  }
 });
 
-toPageOne.addEventListener('click', () => {
-  //Show first page
-  grassModalPageOne.classList.remove('hidden');
-  grassModalPageOneNavigation.classList.remove('hidden');
-  //Hide second page
-  grassModalPageTwo.classList.add('hidden');
-  grassModalPageTwoNavigation.classList.add('hidden');
+//#region Grass Modal Navigation
+toPageTwo.forEach((button) => {
+  button.addEventListener('click', () => {
+    //Hide first page
+    grassModalPageOne.classList.add('hidden');
+    grassModalPageOneNavigation.classList.add('hidden');
+    //Show second page
+    grassModalPageTwo.classList.remove('hidden');
+    grassModalPageTwoNavigation.classList.remove('hidden');
+  });
 });
+
+toPageOne.forEach((button) => {
+  button.addEventListener('click', () => {
+    //Show first page
+    grassModalPageOne.classList.remove('hidden');
+    grassModalPageOneNavigation.classList.remove('hidden');
+    //Hide second page
+    grassModalPageTwo.classList.add('hidden');
+    grassModalPageTwoNavigation.classList.add('hidden');
+  });
+});
+//#endregion
+
+//#endregion
 
