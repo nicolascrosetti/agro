e3e480bd90594b3e70afece6d6ae6c5a1922a2ae
Nicolas <nccrosetti@gmail.com>
2023-04-25 08:42:17 -0300

    add shadows

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,30 @@
 /** @type {import('tailwindcss').Config} */
+const plugin = require('tailwindcss/plugin');
+
 module.exports = {
   content: ["./dist/*.{html,js}"],
   theme: {
     extend: {
       fontFamily: {
         inter: ['Inter']
+      },
+      textShadow: {
+        sm: '0 1px 2px var(--tw-shadow-color)',
+        DEFAULT: '0 2px 4px var(--tw-shadow-color)',
+        lg: '0 8px 16px var(--tw-shadow-color)',
       }
     },
   },
-  plugins: [],
+  plugins: [
+    plugin(function ({ matchUtilities, theme }) {
+      matchUtilities(
+        {
+          'text-shadow': (value) => ({
+            textShadow: value,
+          }),
+        },
+        { values: theme('textShadow') }
+      )
+    }),
+  ],
 }
