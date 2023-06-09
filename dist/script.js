65c3876a82ec6cda1555ed32fd75d76497ec38f6
Nicolas <nccrosetti@gmail.com>
2023-06-08 21:04:22 -0300

    modify gallery

--- dist/script.js
+++ dist/script.js
@@ -30,8 +30,9 @@ const grassModalPageTwoNavigation = document.getElementById('grass-modal-page-tw
 window.onload = function() {
   new Glide('.glide', {
     type: 'carousel',
-    perView: 3,
+    perView: 3.5,
     focusAt: 'center',
+    gap: 40,
     breakpoints: {
       768: {
         perView: 1
